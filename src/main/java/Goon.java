9754b2c474e9b051b75165049022a5cf2de92c3b
fieash <wongyuzhang45@gmail.com>
2025-01-24 00:30:56 +0800

    level3 mark as done

--- src/main/java/Goon.java
+++ src/main/java/Goon.java
@@ -8,7 +8,7 @@ public class Goon {
     }
 
     public static void main(String[] args) {
-        System.out.println("" +
+        System.out.println(
                 "  ________                    __________        __   \n" +
                         " /  _____/  ____   ____   ____\\______   \\ _____/  |_ \n" +
                         "/   \\  ___ /  _ \\ /  _ \\ /    \\|    |  _//  _ \\   __\\\n" +
@@ -16,8 +16,8 @@ public class Goon {
                         " \\______  /\\____/ \\____/|___|  /______  /\\____/|__|  \n" +
                         "        \\/                   \\/       \\/              \n" +
                 "____________________________________________________________\n" +
-                " Hello! I'm GoonBot\n" +
-                " What can I do for you?\n" +
+                " Good morning Gooner, this is GoonBot\n" +
+                " How may I increase your levels of goon today?\n" +
                 "____________________________________________________________\n");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
@@ -26,17 +26,26 @@ public class Goon {
 
         while (!input.equals("bye")) {
             if(input.equals("list")) {
-                System.out.println("\t____________________________________________________________");
+                System.out.println("\t____________________________________________________________\n" +
+                        "Here are the tasks in your list:");
                 for(String s : listInputs){
                     System.out.println("\t" + s);
                 }
                 System.out.println("\t____________________________________________________________\n");
+            } else if(input.length() > 4 && input.substring(0, 4).equals("mark")){
+                int mark = input.charAt(5) - '0';
+                System.out.println("\t____________________________________________________________\n" +
+                        "Nice! I've marked this task as done:");
+                String stringToMark = listInputs.get(mark-1);
+                stringToMark = stringToMark.substring(0,3) + "x" + stringToMark.substring(4);
+                listInputs.set(mark-1, stringToMark);
+                System.out.println("\t"+ listInputs.get(mark-1).substring(2));
+                System.out.println("\t____________________________________________________________\n");
             }else {
                 formattedPrint("added: "+ input);
-                listInputs.add(counter + ". " + input);
+                listInputs.add(counter + ".[ ] " + input);
                 counter++;
             }
-
             input = scanner.nextLine();
         }
 
