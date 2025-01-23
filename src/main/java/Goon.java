f392427f43d7b07d112db2c3b0702143043e707a
fieash <wongyuzhang45@gmail.com>
2025-01-24 00:11:24 +0800

    level 2 list adding

--- src/main/java/Goon.java
+++ src/main/java/Goon.java
@@ -1,27 +1,46 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Goon {
+    public static void formattedPrint(String input) {
+        System.out.println("\t____________________________________________________________\n"
+                + "\t"+ input + "\n\t____________________________________________________________\n");
+    }
+
     public static void main(String[] args) {
         System.out.println("" +
-                " ________                      \n" +
-                " /  _____/  ____   ____   ____  \n" +
-                "/   \\  ___ /  _ \\ /  _ \\ /    \\ \n" +
-                "\\    \\_\\  (  <_> |  <_> )   |  \\\n" +
-                " \\______  /\\____/ \\____/|___|  /\n" +
-                "        \\/                   \\/ \n" +
+                "  ________                    __________        __   \n" +
+                        " /  _____/  ____   ____   ____\\______   \\ _____/  |_ \n" +
+                        "/   \\  ___ /  _ \\ /  _ \\ /    \\|    |  _//  _ \\   __\\\n" +
+                        "\\    \\_\\  (  <_> |  <_> )   |  \\    |   (  <_> )  |  \n" +
+                        " \\______  /\\____/ \\____/|___|  /______  /\\____/|__|  \n" +
+                        "        \\/                   \\/       \\/              \n" +
                 "____________________________________________________________\n" +
                 " Hello! I'm GoonBot\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        ArrayList<String> listInputs = new ArrayList<String>();
+        int counter = 1;
+
         while (!input.equals("bye")) {
-            System.out.println("\n\t____________________________________________________________\n"
-            + "\t" + input + "\n\t____________________________________________________________\n");
+            if(input.equals("list")) {
+                System.out.println("\t____________________________________________________________");
+                for(String s : listInputs){
+                    System.out.println("\t" + s);
+                }
+                System.out.println("\t____________________________________________________________\n");
+            }else {
+                formattedPrint("added: "+ input);
+                listInputs.add(counter + ". " + input);
+                counter++;
+            }
+
             input = scanner.nextLine();
         }
-        System.out.println("\t____________________________________________________________\n " +
-                "\tBye. Hope to see you again soon!" +
-                "\n\t____________________________________________________________\n");
+
+        formattedPrint("Bye. Hope to see you again soon!");
     }
+
 }
